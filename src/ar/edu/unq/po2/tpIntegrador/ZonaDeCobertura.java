ae9f0e75ec765e9cdefa65833181303b1cfb3474
Administrator <Administrator@DESKTOP-FI1EJ2S>
2022-06-09 19:43:48 -0300

    zona de cobertura completa y sus test

--- src/ar/edu/unq/po2/tpIntegrador/ZonaDeCobertura.java
+++ src/ar/edu/unq/po2/tpIntegrador/ZonaDeCobertura.java
@@ -1,48 +1,37 @@
 package ar.edu.unq.po2.tpIntegrador;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.function.Supplier;
 
-public class ZonaDeCobertura extends Observable implements Observer {
+import org.mockito.ArgumentMatchers;
+
+public class ZonaDeCobertura  implements Observer {
 	
-	private List<Organizacion> organizacionesRegistada;
-	private List<Muestra> muestrasEnZona;
+	private List<Organizacion> organizacionesRegistada= new ArrayList<Organizacion>();
+	private List<Muestra> muestrasEnZona= new ArrayList<Muestra>();
+	
+
 	private SistemaDeMuestras sistema;
 	private String nombre;
 	private Ubicacion epicentro;
 	private Double radio;
 	private AuxiliarDeUbicacion aux;
 	
-	@SuppressWarnings("deprecation")
-	public ZonaDeCobertura(String nombre, Ubicacion epicentro, Double radio) {
+	
+	public ZonaDeCobertura(String nombre, Ubicacion epicentro, Double radio, SistemaDeMuestras sistema) {
 		super();
 		this.nombre = nombre;
 		this.epicentro = epicentro;
 		this.radio = radio;
 		this.aux= new AuxiliarDeUbicacion();
+		this.sistema= sistema;
 		this.sistema.addObserver(this);
 	}
 	
-	public void reportarCarga(Muestra muestra) {
-		
-		if(this.muestraPertenece(muestra)) {
-			this.muestrasEnZona.add(muestra);
-			organizacionesRegistada.forEach(orga -> orga.cargarDeMuestra(muestra));
-		}
-		
-		
-	}
-	
-	public void reportarValidacion(Muestra muestra) {
-
-		if(this.muestraPertenece(muestra)) {
-			this.muestrasEnZona.add(muestra);
-			organizacionesRegistada.forEach(orga -> orga.validacionDeMuestra(muestra));
-		}
-		
-	}
+	// geters y setters
 	
 	
 	public Ubicacion epicentro() {
@@ -56,7 +45,15 @@ public class ZonaDeCobertura extends Observable implements Observer {
 		return this.nombre;
 	}
 
-
+	public List<Organizacion> organizacionesRegistradas() {
+		
+		return this.organizacionesRegistada;
+	}
+	public List<Muestra> getMuestrasEnZona() {
+		return  this.muestrasEnZona;
+	}
+	
+	// Requerimientos ed la Clase
 	public boolean muestraPertenece(Muestra muestra1) {
 		// TODO Auto-generated method stub
 		return this.aux.distanciaEntre(this.epicentro(), muestra1.ubicacion()) <= this.radio ;
@@ -66,10 +63,46 @@ public class ZonaDeCobertura extends Observable implements Observer {
 		return zona2.radio() > (this.aux.distanciaEntre(this.epicentro(), zona2.epicentro()) - this.radio() )  ;
 		
 	}
+	
+	// Metodos de Implem,emtacion del patron Observer
+	
+	public void cargarMuestra(Muestra m) {
+		this.muestrasEnZona.add(m);
+	}
+	
+	public void reportarCarga(Muestra muestra) {
+		
+		if(this.muestraPertenece(muestra)) {
+			this.muestrasEnZona.add(muestra);
+			organizacionesRegistada.forEach(orga -> orga.cargaDeMuestra(muestra));
+		}
+		
+		
+	}
+	
+	public void reportarValidacion(Muestra muestra) {
+
+		if(this.muestraPertenece(muestra)) {
+			this.muestrasEnZona.add(muestra);
+			organizacionesRegistada.forEach(orga -> orga.validacionDeMuestra(muestra));
+		}
+		
+	}
+
+
+	public void agregarOrganizacionObservadora(Organizacion o) {
+		this.organizacionesRegistada.add(o);
+	}
+	
+	public void eliminarOrganizacion(Organizacion o) {
+		this.organizacionesRegistada.remove(o);
+	}
 
 	@Override
 	public void update(Observable o, Object arg) {
 		// TODO Auto-generated method stub
 		
 	}
+
+	
 }
